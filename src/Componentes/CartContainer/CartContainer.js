119919b1b0df429d5852cf8237d9733f2d86bbf9
Martino Baleato <martinogbaleato@gmail.com>
2022-09-22 13:52:27 -0300

    utlimo desafio

--- src/Componentes/CartContainer/CartContainer.js
+++ src/Componentes/CartContainer/CartContainer.js
@@ -3,28 +3,12 @@ import { useContext } from 'react'
 import { CartContext } from '../../Context/CartContext'
 import { Item } from '../Item/Item'
 import {Link as LinkBoostrap} from "react-router-dom"
-import { db } from "../../Utils/firebase"
-import { collection, addDoc } from 'firebase/firestore'
+import { Formulario } from '../Formulario/Formulario'
 
 
 
 export const CartContainer = () => {
-    const { productCartList, clearCart,deleteProduct,getTotalPrice } = useContext(CartContext);
-    const [idOrder, setIdOrder] = useState ("");
-    const sendOrder =(e)=>{
-        e.preventDefault();
-    const order ={
-        buyer:{
-            name: e.target[0].value,
-            phone: e.target[1].value,
-            email: e.target[2].value
-        },
-        items: productCartList,
-        total: getTotalPrice()
-    }
-    const queryRef = collection(db,"orders")
-    addDoc(queryRef,order).then(respuesta=>setIdOrder(respuesta.id))
-    }
+    const { productCartList, clearCart,deleteProduct } = useContext(CartContext);
 
     
     if (productCartList.length === 0) {
@@ -64,13 +48,7 @@ export const CartContainer = () => {
 
       <button onClick={()=>clearCart()}>Vaciar Carrito</button> 
       <p>Precio total ${getTotalPrice()}</p> 
-            <form onSubmit={sendOrder}>
-                <input type="text" placeholder="nombre"></input>
-                <input type="text" placeholder="telefono"></input>
-                <input type="email" placeholder="email"></input>
-                <button type="submit">Terminar Compra</button>
-            </form>
-            <p>Tu codigo para el seguimiento del pedido: {idOrder}</p>
+      <Formulario/>
         </>
     )
 }
